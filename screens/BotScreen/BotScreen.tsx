93fcd3a184920ac2b4500b21e1f347dce719f3c5
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-09-12 14:37:23 -0400

    UI updates

--- screens/BotScreen/BotScreen.tsx
+++ screens/BotScreen/BotScreen.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Dimensions, View, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
-import { Avatar } from 'react-native-paper';
+import React, { useState } from 'react';
+import { Dimensions, KeyboardAvoidingView, View, SafeAreaView, ScrollView, StyleSheet, Text } from 'react-native';
+import { Avatar, Button, IconButton, TextInput } from 'react-native-paper';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 const DoctroLogo = require('../../assets/app-media/icon.png');
 
 const { height } = Dimensions.get('screen');
 
 
 export default function BotScreen({ onLayoutRootView }: { onLayoutRootView: any }) {
+
+    const [showSoftInputFocus, setShowSoftInputFocus] = useState(true);
 
     return (
         <View onLayout={onLayoutRootView} style={styles.screenContainer}>
@@ -24,9 +27,16 @@ export default function BotScreen({ onLayoutRootView }: { onLayoutRootView: any 
                 </ScrollView>
             </SafeAreaView>
             <View style={styles.bottomBar}>
-                <Text style={{ color: '#ffffff' }}>
-                    Bottom View 
-                </Text>
+                <TextInput 
+                    activeOutlineColor="#ffffff"
+                    left={<TextInput.Icon icon="microphone" onPress={() => setShowSoftInputFocus(false)} size={35} />}
+                    mode="outlined"
+                    numberOfLines={1}
+                    onTouchStart={() => setShowSoftInputFocus(true)}
+                    placeholder="Question?"
+                    showSoftInputOnFocus={showSoftInputFocus}
+                    style={{ flex: 1, flexWrap: 'nowrap', width: '100%', }}
+                />
             </View>
         </View>
     );
@@ -44,17 +54,24 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     bottomBar: {
+        alignItems: 'center',
         backgroundColor: '#002244',
         bottom: 0,
         display: 'flex',
         flex: 1,
         flexDirection: 'row',
-        height: 75,
+        height: 150,
         left: 0,
-        marginTop: 700,
+        lineHeight: 'normal',
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
         position: 'absolute',
         right: 0,
         width: '100%',
+    },
+    btn: {
+        color: '#fffff',
     },
     chatContainer: {
         alignItems: 'center',
@@ -62,6 +79,7 @@ const styles = StyleSheet.create({
         flex: 3,
         paddingBottom: 40,
         overflow: 'scroll',
+        width: '100%',
     },
     innerChatContainer: {
         paddingTop: 100,
